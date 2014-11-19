77a1d9ecfb33b17802498ef1a6020e296fff073f
Yusuke SHIMA <mashi4729@gmail.com>
2014-11-19 13:52:09 +0900

    init

---
+++ actor_test_NOLPC/src/actor_test_NOLPC.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : actor_test_NOLPC.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
